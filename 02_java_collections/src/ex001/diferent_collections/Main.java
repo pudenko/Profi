aeb7070cb026f58029cec1394e8485bda5044e37
pudikq@gmail.com <pudikq@gmail.com>
2021-04-14 22:40:21 +0300

    expert

---
+++ 02_java_collections/src/ex001/diferent_collections/Main.java
@@ -0,0 +1,13 @@
+package ex001.diferent_collections;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> fi = new ArrayList<>(100);
+        List<String> fa = new LinkedList<>();
+        List<String> fo = new Vector<>();
+
+        Queue<String> far = new LinkedList<>();
+    }
+}
