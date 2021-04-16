cc8571fa6744972a21402e99fc3a63a3b370ee6e
pudikq@gmail.com <pudikq@gmail.com>
2021-04-16 21:44:54 +0300

    expert

--- 03_java_collections/src/ex001/diferent_collections/Main.java
+++ 03_java_collections/src/ex001/diferent_collections/Main.java
@@ -2,6 +2,7 @@ package ex001.diferent_collections;
 
 
 import java.util.*;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +11,15 @@ public class Main {
         List<String> fo = new Vector<>();
 
         Queue<String> far = new LinkedList<>();
+        Queue<String> frog = new PriorityQueue<>();
+        Deque<String> first = new LinkedList<>();
+
+        Set<String> seter = new HashSet<>();
+        SortedSet<String> stringer = new TreeSet<>();
+        NavigableSet<String> huinia = new TreeSet<>();
+
+        Map<String,Integer> yyy = new HashMap<>();
+        SortedMap<String,Integer> yyy1 = new TreeMap<>();
+        NavigableMap<String,Integer> yyy2 = new TreeMap<>();
     }
 }
